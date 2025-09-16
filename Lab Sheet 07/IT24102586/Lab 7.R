9171ecfc1947e096c8c0080f6fbee9398d8bf577
it24102586 <it24102586@my.sliit.lk>
2025-09-16 11:57:31 +0530

    Lab 07

---
+++ Lab Sheet 07/IT24102586/Lab 7.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\DELL\\Desktop\\IT24102586")
+
+punif(25,min = 0, max = 40,lower.tail = TRUE)-punif(10,min = 0,max = 40,lower.tail = TRUE)
+
+pexp(2,rate = 0.333,lower.tail = TRUE)
+
+1-pnorm(130,mean = 100,sd = 15,lower.tail = TRUE)
+
+qnorm(0.95,mean = 100,sd=15,lower.tail = TRUE)
+
